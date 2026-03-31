fbadf582d4bc1a736c9f763e345d99ee634693ca
adechazal1 <adechazal@udesa.edu.ar>
2026-03-31 01:07:41 -0300

    Hice la estructura del MovieCard, pero esta incompleto. Nose como seguir, en la tuto pregunto lo q tengo anotado.

--- src/Componentes/Movie_Card/Movie_Card.js
+++ src/Componentes/Movie_Card/Movie_Card.js
@@ -1,16 +1,48 @@
 import React, { Component } from "react";
-class MovieCard extends Component { 
-    constructor(props){
+import { Link } from "react-router-dom";
+
+class MovieCard extends Component {
+    constructor(props) {
         super(props)
-        
-    }
-    render(){
-        return(
-            <article className="Card">
-                
-            </article>
-        );            
-        
+        this.state = {
+            mostrarDescripcion: false,
+            detalle: "", //no se si esta bien
+            Esfavorito: false,
+            favoritos: []
+        }
     }
 
+    mostrarDescripcion() {
+        this.setState({
+            mostrarDescripcion: true,
+        })
+    };
+    iraDetalle(){
+        this.setState({
+           //preguntar como hacer esta funcion y el boton en el render 
+        }) 
+    };
+    AgregarAFavoritos(){
+        this.setState({
+            Esfavorito: true,
+            favoritos: this.state.push
+        })
+    };
+
+render() {
+    return (
+        <article className="Card">
+            <img src={this.props.imagen} alt={this.props.nombre} />
+            <h1 className="titulo">{this.props.nombre}</h1>
+            <p className="descripcion">{this.props.descripcion}</p>
+            <button onClick={() => this.mostrarDescripcion()} className="mostrarDescripcionBoton">
+                {this.state.mostrarDescripcion ? "ver menos" : "ver descripcion"}
+            </button>
+            //falta el boton de ir a detalle
+            <button onClick={() => this.AgregarAFavoritos()}>{this.state.Esfavorito ? "Eliminar de favoritos":"Agregar a favoritos"}</button>
+
+        </article>
+    );
+
+}
 }
