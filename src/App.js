9234d7c04ccdc72c2965778489f6da66501aa609
fermin-saralegui <fsaralegui@udesa.edu.ar>
2026-04-01 19:18:36 -0300

    Termino de configurar el fetch de la Api en movie card y home

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from "react-router-dom";
 import Header from "./Componentes/Header/Header"
 import Footer from "./Componentes/Footer/Footer"
 import Home from "./Screens/Home/Home"
-
+const apikey='7aa285e4357da2124c14f7534bfc86a0'
 function App() {
   let item = [
     {
@@ -24,7 +24,7 @@ function App() {
     }
   ];
   
-fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=7aa285e4357da2124c14f7534bfc86a0')
+fetch('https://api.themoviedb.org/3/movie/now_playing?api_key='+apikey)
   .then(res => res.json())
   .then(res => console.log(res))
   .catch(err => console.error(err));
--- src/Componentes/Movie_Card/Movie_Card.js
+++ src/Componentes/Movie_Card/Movie_Card.js
@@ -3,46 +3,56 @@ import { Link } from "react-router-dom";
 
 class MovieCard extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             mostrarDescripcion: false,
-            detalle: "", //no se si esta bien
-            Esfavorito: false,
-            favoritos: []
-        }
+            esFavorito: false
+        };
     }
 
     mostrarDescripcion() {
         this.setState({
-            mostrarDescripcion: true,
-        })
-    };
-    iraDetalle(){
+            mostrarDescripcion: !this.state.mostrarDescripcion
+        });
+    }
+
+    agregarAFavoritos() {
         this.setState({
-           //preguntar como hacer esta funcion y el boton en el render 
-        }) 
-    };
-    AgregarAFavoritos(){
-        this.setState({
-            Esfavorito: true,
-            favoritos: this.state.push
-        })
-    };
+            esFavorito: !this.state.esFavorito
+        });
+    }
 
-render() {
-    return (
-        <article className="Card">
-            <img src={this.props.imagen} alt={this.props.nombre} />
-            <h1 className="titulo">{this.props.nombre}</h1>
-            <p className="descripcion">{this.props.descripcion}</p>
-            <button onClick={() => this.mostrarDescripcion()} className="mostrarDescripcionBoton">
-                {this.state.mostrarDescripcion ? "ver menos" : "ver descripcion"}
-            </button>
-            //falta el boton de ir a detalle
-            <button onClick={() => this.AgregarAFavoritos()}>{this.state.Esfavorito ? "Eliminar de favoritos":"Agregar a favoritos"}</button>
+    render() {
+        return (
+            <article className="Card">
+                <img
+                    src={`https://image.tmdb.org/t/p/w342/${this.props.imagen}`}
+                    alt={this.props.nombre}
+                />
 
-        </article>
-    );
+                <h1 className="titulo">{this.props.nombre}</h1>
 
+                {this.state.mostrarDescripcion ? (
+                    <p className="descripcion">{this.props.descripcion}</p>
+                ) : null}
+
+                <button
+                    onClick={() => this.mostrarDescripcion()}
+                    className="mostrarDescripcionBoton"
+                >
+                    {this.state.mostrarDescripcion ? "Ver menos" : "Ver descripción"}
+                </button>
+
+                <Link to={`/detalle/${this.props.id}`}>
+                    <button>Ir a detalle</button>
+                </Link>
+
+                <button onClick={() => this.agregarAFavoritos()}>
+                    {this.state.esFavorito ? "Eliminar de favoritos" : "Agregar a favoritos"}
+                </button>
+            </article>
+        );
+    }
 }
-}
+
+export default MovieCard;
